5da4f8a7f538525de4d5d35e9369bcaa1d031b9f
Camila Germano Rodrigues <camyrodrigues23@gmail.com>
2026-03-18 17:20:56 -0300

    feat:finalizar tela de registro e adicionar redirecionamentos

--- minhas-assinaturas/src/App.tsx
+++ minhas-assinaturas/src/App.tsx
@@ -1,14 +1,19 @@
+import { Routes, Route } from 'react-router-dom'
 import './index.css'
-//import Dashboard from './pages/Dashboard'
 import Register from './pages/Register'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
 
 
 function App() {
-
-  return(
-    //<Dashboard/>
-    <Register/>
-)
+  return (
+    <Routes>
+      <Route path="/" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  )
 }
 
 export default App
--- minhas-assinaturas/src/pages/Register.tsx
+++ minhas-assinaturas/src/pages/Register.tsx
@@ -1,10 +1,18 @@
 import { labelStyle, inputStyle, buttonStyle } from "../styles/formStyles"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+
 
 function Register() {
+
+  const navigate = useNavigate()
+
+  function handleSubmit(e: React.FormEvent) {
+  e.preventDefault()
+  navigate("dashboard")
+}
   return (
     
-      <div className=" min-h-screen bg-gradient-to-r from-[#020617] via-[#0a1a2f] to-[#020617] flex flex-col justify-center items-center">
+      <div className=" min-h-screen bg-gradient-to-r from-[#020617] via-[#213B63] to-[#020617] flex flex-col justify-center items-center">
 
         <div className="w-full max-w-md bg-[#0f172a] p-8 rounded-2xl border border-slate-800 shadow-xl flex-col justify-center items-center">
           <div className="flex justify-center mb-4">
@@ -18,20 +26,20 @@ function Register() {
         <h1 className="flex flex-row justify-center text-2xl text-text font-bold">Criar Conta</h1>
         <h3 className="flex flex-row justify-center text-gray-400">Cadastre-se para começar a controlar suas assinaturas</h3>
 
-        <form className="w-full flex flex-col">
+        <form className="w-full flex flex-col p-8" onSubmit={handleSubmit}>
           <label className={labelStyle} htmlFor="name">Nome Completo:</label>
           <input className={inputStyle} type="text" id="name" placeholder="Seu nome" />
 
           <label className={labelStyle} htmlFor="email">Seu e-mail:</label>
-          <input className={inputStyle} type="email" id="email" placeholder="E-mail" />
+          <input className={inputStyle} type="email" id="email" placeholder="seu@email.com" />
 
           <label className={labelStyle} htmlFor="password">Senha:</label>
-          <input className={inputStyle} type="password" id="password" placeholder="Senha" />
+          <input className={inputStyle} type="password" id="password" placeholder="******" />
 
           <label className={labelStyle} htmlFor="confirmPassword">Confirmar senha:</label>
-          <input className={inputStyle} type="password" id="confirmPassword" placeholder="Confirmar senha" />
+          <input className={inputStyle} type="password" id="confirmPassword" placeholder="******" />
 
-          <button className={buttonStyle} type="submit">Cadastrar</button>
+          <button className={buttonStyle} type="submit" >Cadastrar</button>
 
           <p className="text-gray-400 text-sm text-center mt-4"> Já tem conta?{" "}
              <Link to="/login" className="text-teal-400 hover:underline">Faça login</Link>
